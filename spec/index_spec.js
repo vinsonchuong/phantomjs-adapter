4761fdba4925a42c1a68fef39f0eceb17a48a7b9
Vinson Chuong <vinsonchuong@gmail.com>
2016-08-28 13:42:30 -0700

    Use test-inject for cleaning up test dependencies

--- spec/index_spec.js
+++ spec/index_spec.js
@@ -1,5 +1,6 @@
 import {childProcess} from 'node-promise-es6';
 import {catchError} from 'jasmine-es6';
+import register from 'test-inject';
 import Directory from 'directory-helpers';
 import {path as phantomJsPath} from 'phantomjs-prebuilt';
 import PhantomJS from 'phantomjs-adapter';
@@ -13,34 +14,16 @@ async function phantomPids() {
   }
 }
 
-function withHtml(html, test) {
-  return async () => {
-    const directory = new Directory('temp');
-    const browser = new PhantomJS();
-    try {
-      await directory.write({'index.html': html});
-      await browser.open(`file://${directory.path('index.html')}`);
-      await test(browser, directory);
-    } finally {
-      await browser.exit();
-      await directory.remove();
-    }
-  };
-}
-
-function withBrowser(test) {
-  return async () => {
-    const directory = new Directory('temp');
-    const browser = new PhantomJS();
-    try {
-      await directory.create();
-      await test(browser, directory);
-    } finally {
-      await browser.exit();
-      await directory.remove();
-    }
-  };
-}
+const inject = register({
+  temp: {
+    setUp: () => new Directory('temp'),
+    tearDown: async (temp) => await temp.remove()
+  },
+  browser: {
+    setUp: () => new PhantomJS(),
+    tearDown: async (browser) => await browser.exit()
+  }
+});
 
 describe('phantomjs-adapter', () => {
   it('can kill the phantomjs process', async () => {
@@ -50,165 +33,179 @@ describe('phantomjs-adapter', () => {
     expect(await phantomPids()).not.toContain(browser.process.pid);
   });
 
-  it('can open a URL', withHtml(
-    `
-    <!doctype html>
-    <meta charset="utf-8">
-    <title>Hello World!</title>
-    `,
-    async (browser) => {
-      expect(await browser.title()).toContain('Hello World!');
-    }
-  ));
+  it('can open a URL', inject(async ({temp, browser}) => {
+    await temp.write({
+      'index.html': `
+        <!doctype html>
+        <meta charset="utf-8">
+        <title>Hello World!</title>
+      `
+    });
+    await browser.open(`file://${temp.path('index.html')}`);
+    expect(await browser.title()).toContain('Hello World!');
+  }));
 
-  it('can read information about a DOM element', withHtml(
-    `
-    <!doctype html>
-    <meta charset="utf-8">
-    <div
-      id="root"
-      class="container"
-      style="position: absolute; top: 0; left: 0; width: 200px; height: 100px;"
-    >
-      Hello World!
-    </div>
-    <input value="text">
-    `,
-    async (browser) => {
-      const div = await browser.find('div');
-      expect(div.textContent.trim()).toBe('Hello World!');
-      expect(div.attributes.id).toBe('root');
-      expect(div.attributes.class).toBe('container');
-      expect(div.boundingClientRect).toEqual({
-        top: 0,
-        right: 200,
-        bottom: 100,
-        left: 0,
-        width: 200,
-        height: 100
-      });
+  it('can read information about a DOM element', inject(async ({temp, browser}) => {
+    await temp.write({
+      'index.html': `
+        <!doctype html>
+        <meta charset="utf-8">
+        <div
+          id="root"
+          class="container"
+          style="position: absolute; top: 0; left: 0; width: 200px; height: 100px;"
+        >
+          Hello World!
+        </div>
+        <input value="text">
+      `
+    });
+    await browser.open(`file://${temp.path('index.html')}`);
 
-      const input = await browser.find('input');
-      expect(input.value).toBe('text');
-    }
-  ));
+    const div = await browser.find('div');
+    expect(div.textContent.trim()).toBe('Hello World!');
+    expect(div.attributes.id).toBe('root');
+    expect(div.attributes.class).toBe('container');
+    expect(div.boundingClientRect).toEqual({
+      top: 0,
+      right: 200,
+      bottom: 100,
+      left: 0,
+      width: 200,
+      height: 100
+    });
 
-  it('can find a DOM element by text', withHtml(
-    `
-    <!doctype html>
-    <meta charset="utf-8">
-    <span>Hello</span>
-    <span>World!</span>
-    `,
-    async (browser) => {
-      const span = await browser.find('span', {text: 'World!'});
-      expect(span.textContent).toBe('World!');
-    }
-  ));
+    const input = await browser.find('input');
+    expect(input.value).toBe('text');
+  }));
 
-  it('returns null if a DOM element does not exist', withHtml(
-    `
-    <!doctype html>
-    <meta charset="utf-8">
-    `,
-    async (browser) => {
-      const missing = await browser.find('div');
-      expect(missing).toBe(null);
-    }
-  ));
+  it('can find a DOM element by text', inject(async ({temp, browser}) => {
+    await temp.write({
+      'index.html': `
+        <!doctype html>
+        <meta charset="utf-8">
+        <span>Hello</span>
+        <span>World!</span>
+      `
+    });
+    await browser.open(`file://${temp.path('index.html')}`);
 
-  it('can wait until an element is found', withHtml(
-    `
-    <!doctype html>
-    <meta charset="utf-8">
-    <script>
-      setTimeout(function() {
-        var p = document.createElement('p');
-        p.textContent = 'Hello World!';
-        document.body.appendChild(p);
-      }, 1000);
-    </script>
-    `,
-    async (browser) => {
-      const paragraph = await browser.find('p', {wait: 2000});
-      expect(paragraph.textContent).toBe('Hello World!');
-    }
-  ));
+    const span = await browser.find('span', {text: 'World!'});
+    expect(span.textContent).toBe('World!');
+  }));
 
-  it('returns null if an element cannot be found within the time limit', withHtml(
-    `
-    <!doctype html>
-    <meta charset="utf-8">
-    `,
-    async (browser) => {
-      const missing = await browser.find('p', {wait: 1000});
-      expect(missing).toBe(null);
-    }
-  ));
+  it('returns null if a DOM element does not exist', inject(async ({temp, browser}) => {
+    await temp.write({
+      'index.html': `
+        <!doctype html>
+        <meta charset="utf-8">
+      `
+    });
+    await browser.open(`file://${temp.path('index.html')}`);
 
-  it('can click on a DOM element', withHtml(
-    `
-    <!doctype html>
-    <meta charset="utf-8">
-    <button onClick="root.textContent = 'Hello World!';">
-      Button
-    </button>
-    <div id="root"></div>
-    `,
-    async (browser) => {
-      const button = await browser.find('button');
-      await button.click();
+    const missing = await browser.find('div');
+    expect(missing).toBe(null);
+  }));
 
-      const root = await browser.find('#root');
-      expect(root.textContent).toBe('Hello World!');
-    }
-  ));
+  it('can wait until an element is found', inject(async ({temp, browser}) => {
+    await temp.write({
+      'index.html': `
+        <!doctype html>
+        <meta charset="utf-8">
+        <script>
+          setTimeout(function() {
+            var p = document.createElement('p');
+            p.textContent = 'Hello World!';
+            document.body.appendChild(p);
+          }, 1000);
+        </script>
+      `
+    });
+    await browser.open(`file://${temp.path('index.html')}`);
 
-  it('can fill in a DOM element', withHtml(
-    `
-    <!doctype html>
-    <meta charset="utf-8">
-    <input>
-    `,
-    async (browser) => {
-      let input = await browser.find('input');
-      await input.fillIn('Hello World!');
+    const paragraph = await browser.find('p', {wait: 2000});
+    expect(paragraph.textContent).toBe('Hello World!');
+  }));
 
-      input = await browser.find('input');
-      expect(input.value).toBe('Hello World!');
-    }
-  ));
+  it('returns null if an element cannot be found within the time limit', inject(async ({temp, browser}) => {
+    await temp.write({
+      'index.html': `
+        <!doctype html>
+        <meta charset="utf-8">
+      `
+    });
+    await browser.open(`file://${temp.path('index.html')}`);
 
-  it('throws an exception when it fails to open a page', withBrowser(async (browser) => {
+    const missing = await browser.find('p', {wait: 1000});
+    expect(missing).toBe(null);
+  }));
+
+  it('can click on a DOM element', inject(async ({temp, browser}) => {
+    await temp.write({
+      'index.html': `
+        <!doctype html>
+        <meta charset="utf-8">
+        <button onClick="root.textContent = 'Hello World!';">
+          Button
+        </button>
+        <div id="root"></div>
+      `
+    });
+    await browser.open(`file://${temp.path('index.html')}`);
+
+    const button = await browser.find('button');
+    await button.click();
+
+    const root = await browser.find('#root');
+    expect(root.textContent).toBe('Hello World!');
+  }));
+
+  it('can fill in a DOM element', inject(async ({temp, browser}) => {
+    await temp.write({
+      'index.html': `
+        <!doctype html>
+        <meta charset="utf-8">
+        <input>
+      `
+    });
+    await browser.open(`file://${temp.path('index.html')}`);
+
+    let input = await browser.find('input');
+    await input.fillIn('Hello World!');
+
+    input = await browser.find('input');
+    expect(input.value).toBe('Hello World!');
+  }));
+
+  it('throws an exception when it fails to open a page', inject(async ({browser}) => {
     expect(await catchError(browser.open('http://foo')))
       .toContain('Failed to open http://foo');
   }));
 
-  it('throws an exception when it opens a page with a JavaScript exception', withBrowser(async (browser, directory) => {
-    await directory.write({
+  it('throws an exception when it opens a page with a JavaScript exception', inject(async ({temp, browser}) => {
+    await temp.write({
       'index.html': `
-      <!doctype html>
-      <meta charset="utf-8">
-      <script>
-        throw new Error('This is a JS error');
-      </script>
+        <!doctype html>
+        <meta charset="utf-8">
+        <script>
+          throw new Error('This is a JS error');
+        </script>
       `
     });
-    expect(await catchError(browser.open(`file://${directory.path('index.html')}`)))
+    expect(await catchError(browser.open(`file://${temp.path('index.html')}`)))
       .toContain('This is a JS error');
   }));
 
-  it('throws an exception when evaluating JS produces an exception', withHtml(
-    `
-    <!doctype html>
-    <meta charset="utf-8">
-    `,
-    async (browser) => {
-      expect(await catchError(
-        browser.evaluate(`
-          throw new Error('This is an error.')
-        `)
-      )).toContain('This is an error.');
-    }
-  ));
+  it('throws an exception when evaluating JS produces an exception', inject(async ({temp, browser}) => {
+    await temp.write({
+      'index.html': `
+        <!doctype html>
+        <meta charset="utf-8">
+      `
+    });
+    await browser.open(`file://${temp.path('index.html')}`);
+    expect(await catchError(
+      browser.evaluate('throw new Error("This is an error.")')
+    )).toContain('This is an error.');
+  }));
 });
--- spec/lib/client_spec.js
+++ spec/lib/client_spec.js
@@ -1,26 +1,28 @@
 import {catchError} from 'jasmine-es6';
+import register from 'test-inject';
 import Directory from 'directory-helpers';
 import Client from 'phantomjs-adapter/lib/client';
 
-function withClient(script, test) {
-  return async () => {
-    const directory = new Directory('temp');
-    await directory.write({'phantom-script.js': script});
-    const phantom = directory.spawn('phantomjs', ['phantom-script.js']);
-    try {
-      const client = new Client(phantom.process);
-      await test(client);
-    } finally {
-      phantom.process.kill();
-      await directory.remove();
+const inject = register({
+  makeClient: class {
+    static setUp() {
+      return async (script) => {
+        this.directory = new Directory('temp');
+        await this.directory.write({'phantom-script.js': script});
+        this.phantom = this.directory.spawn('phantomjs', ['phantom-script.js']);
+        return new Client(this.phantom.process);
+      };
     }
-  };
-}
-
+    static async tearDown() {
+      this.phantom.process.kill();
+      await this.directory.remove();
+    }
+  }
+});
 
 describe('Client', () => {
-  it('sends and receives JSON messages to/from a child process', withClient(
-    `
+  it('sends and receives JSON messages to/from a child process', inject(async ({makeClient}) => {
+    const client = await makeClient(`
       'use strict';
       var system = require('system');
       var request;
@@ -31,17 +33,16 @@ describe('Client', () => {
           result: request
         }));
       }
-    `,
-    async (client) => {
-      const promise1 = client.send('call1', []);
-      const promise2 = client.send('call2', []);
-      expect(await promise1).toEqual({id: 1, method: 'call1', params: []});
-      expect(await promise2).toEqual({id: 2, method: 'call2', params: []});
-    })
-  );
+    `);
 
-  it('receives and propagates exceptions', withClient(
-    `
+    const promise1 = client.send('call1', []);
+    const promise2 = client.send('call2', []);
+    expect(await promise1).toEqual({id: 1, method: 'call1', params: []});
+    expect(await promise2).toEqual({id: 2, method: 'call2', params: []});
+  }));
+
+  it('receives and propagates exceptions', inject(async ({makeClient}) => {
+    const client = await makeClient(`
       'use strict';
       var system = require('system');
       var request = JSON.parse(system.stdin.readLine());
@@ -49,10 +50,7 @@ describe('Client', () => {
         id: request.id,
         error: 'This is an error.'
       }));
-    `,
-    async (client) => {
-      expect(await catchError(client.send('call', [])))
-        .toBe('This is an error.');
-    })
-  );
+    `);
+    expect(await catchError(client.send('call', []))).toBe('This is an error.');
+  }));
 });
